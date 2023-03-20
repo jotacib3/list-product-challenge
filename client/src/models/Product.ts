42788864cb74930ed9d7c4f1769212062d656283
jotacib3 <jotacib3@gmail.com>
2023-03-20 07:30:41 -0400

    feat: add apollo provider

---
+++ client/src/models/Product.ts
@@ -0,0 +1,32 @@
+export interface IProduct {
+    id: string;
+    name: string;
+    sellerName: string;
+    image: string;
+    price: number;
+}
+
+export interface ISimpleProduct extends IProduct {
+    inventory: number;
+}
+
+enum RentalType {
+    NIGHT,
+    HOUR,
+}
+
+export interface IRentableProduct extends IProduct {
+    rentalType: RentalType;
+    availability: string;
+}
+
+export interface ISpaceProduct extends IProduct {
+    location: string;
+    availability: string;
+}
+
+export type IDetailedProduct = ISimpleProduct | IRentableProduct | ISpaceProduct;
+
+export interface IProductResponse {
+    products: IDetailedProduct[]
+}
