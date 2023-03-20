a68732eeaf496bdf68a488bee137b78cf8329ba6
jotacib3 <jotacib3@gmail.com>
2023-03-19 23:13:44 -0400

    feat: create mock api

---
+++ mock_api/src/index.ts
@@ -0,0 +1,114 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  interface Product {
+    id: ID!
+    name: String!
+    sellerName: String!
+    image: String!
+    price: Float!
+  }
+
+  type SimpleProduct implements Product {
+    id: ID!
+    name: String!
+    sellerName: String!
+    image: String!
+    price: Float!
+    inventory: Int!
+  }
+  
+  type RentableProduct implements Product {
+    id: ID!
+    name: String!
+    sellerName: String!
+    image: String!
+    price: Float!
+    rentalType: RentalType!
+    availability: String!
+  }
+
+  type SpaceProduct implements Product {
+    id: ID!
+    name: String!
+    sellerName: String!
+    image: String!
+    price: Float!
+    location: String!
+    availability: String!
+  }
+  
+  enum RentalType {
+    NIGHT
+    HOUR
+  }
+
+  type Query {
+    products: [Product!]!
+  }
+`;
+
+// Resolvers define how to fetch the types defined in your schema.
+// This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Product: {
+        // en este caso decidí implementar el resolver type de esta manera en
+        // lugar de definir una propiedad type para cada producto.
+        __resolveType(product) {
+            if (product.inventory !== undefined) return 'SimpleProduct';
+            if (product.rentalType !== undefined) return 'RentableProduct';
+            return 'SpaceProduct';
+        },
+    },
+    Query: {
+        products: () => [
+            {
+                id: '1',
+                name: 'Bebida',
+                sellerName: 'Vendedor 1',
+                image: 'https://via.placeholder.com/150',
+                price: 10.99,
+                inventory: 100,
+            },
+            {
+                id: '2',
+                name: 'Mesa',
+                sellerName: 'Vendedor 2',
+                image: 'https://via.placeholder.com/150',
+                price: 5.99,
+                rentalType: 'HOUR',
+                availability: '2022-01-01',
+            },
+            {
+                id: '3',
+                name: 'Salón de fiestas',
+                sellerName: 'Vendedor 3',
+                image: 'https://via.placeholder.com/150',
+                price: 200.99,
+                location: 'Ciudad de México',
+                availability: '2022-01-01',
+            },
+        ],
+    },
+};
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+
+// Run index
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
